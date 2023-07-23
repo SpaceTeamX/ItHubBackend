30b82c1c3eea4d74fd4304f161d0942e9f7c6493
Ktoto <nektotoaktoto@protonmail.com>
2023-07-23 23:00:41 +0300

    [Base] User authorization

--- ithub/users/urls.py
+++ ithub/users/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import UserAPIView
 
 urlpatterns = [
-    path("list/", UserAPIView.as_view())
+    path("list/", UserAPIView.as_view()),
+    path("auth/", include("rest_framework.urls"))
 ]
+
