9d12f266c02e3ef22573f765c7857a9edfa000dd
Ktoto <nektotoaktoto@protonmail.com>
2023-08-27 20:01:14 +0300

    Forum models

--- ithub/forum/models.py
+++ ithub/forum/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
 
+
 class Category(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
@@ -8,20 +9,12 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
-class Topic(models.Model):
+class Question(models.Model):
     title = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-
-class Post(models.Model):
-    content = models.TextField()
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Post by {self.author.username} in {self.topic.title}"
